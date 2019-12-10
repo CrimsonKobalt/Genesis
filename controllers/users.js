369806dfa06ad036752c848ef788f6e204911399
Christophe <christophe.db@live.be>
2019-12-10 18:30:41 +0100

    SignUp bug fix

--- controllers/users.js
+++ controllers/users.js
@@ -71,7 +71,7 @@ module.exports = {
             query: {
                 valid: "GyUrQDIze8iGuMD9v8kM",
                 JWT: JSON.stringify(token).match(/.{1,75}/g),
-                voornaam: req.user.first_name
+                voornaam: newUser.first_name
             }
         }));
         //res.render('secret', {name: token.name} );
